2ae925f948b1d7d5969b987c35ccf80b9ee2dd9f
almase <alberto.puche@gmail.com>
2021-11-04 17:00:43 +0100

    Modificado para Jenkins

--- Simple.java
+++ Simple.java
@@ -1,6 +1,6 @@
 
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Prueba de Java");  
+     System.out.println("Prueba de Java. Modificado para Jenkins");  
     }  
 }  
